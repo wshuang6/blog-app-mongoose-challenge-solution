a79d6e4e0458ff4d0f98df1f5ae87252c4359e64
Ramon Reyes <reyesjunk@gmail.com>
2017-03-22 12:57:48 -0400

    "Set up"

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -10,3 +10,32 @@ const {TEST_DATABASE_URL} = require('../config');
 const {app, runServer, closeServer} = require('../server');
 const {BlogPost} = require('../models');
 
+function fakePost(){
+ return {
+   title: faker.company.catchPhrase(),
+   author: {
+     firstName: faker.name.firstName(),
+     lastName: faker.name.lastName(),
+   },
+   content: faker.lorem.sentence()
+
+ } 
+}
+function seedDatabase(){
+  let fakeData = [];
+  for(var i = 0; i<10; i++){
+    fakeData.push(fakePost());
+  }
+  console.log(fakeData);
+  return BlogPost.insertMany(fakeData);
+}
+function tearDownDB(){
+  console.warn("Deleting Database");
+  return mongoose.connection.dropDatabase(); 
+}
+describe('Test BlogPost CRUD functions', ()=>{
+  before(()=>{
+    return runServer(TEST_DATABASE_URL)
+  })
+  
+})
