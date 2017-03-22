8d434c7917085626dc18ef3106c1717042bfe4b7
wshuang6 <seraph617@gmail.com>
2017-03-22 15:08:55 -0400

    working get tests

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -2,67 +2,106 @@ const mocha = require('mocha');
 const chai = require('chai');
 const faker = require('faker');
 const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
 
 const should = chai.should();
-chai.use(chaiHttp);
 
 const {TEST_DATABASE_URL} = require('../config');
 const {app, runServer, closeServer} = require('../server');
 const {BlogPost} = require('../models');
 
-function fakePost(){
- return {
-   title: faker.company.catchPhrase(),
-   author: {
-     firstName: faker.name.firstName(),
-     lastName: faker.name.lastName(),
-   },
-   content: faker.lorem.sentence()
+mongoose.Promise = global.Promise;
+chai.use(chaiHttp);
 
- } 
+function fakePost(){
+  return {
+    title: faker.company.catchPhrase(),
+    author: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+    },
+    content: faker.lorem.sentence()
+  } 
 }
 function seedBlogData(){
   let fakeData = [];
   for(var i = 0; i<10; i++){
     fakeData.push(fakePost());
   }
-  console.log(fakeData);
   return BlogPost.insertMany(fakeData);
 }
 function tearDownDB(){
   console.warn("Deleting Database");
   return mongoose.connection.dropDatabase(); 
 }
-describe('Test BlogPost CRUD functions', ()=>{
-  before(() => {
+describe('Test BlogPost CRUD functions', function () {
+  before(function () {
     return runServer(TEST_DATABASE_URL);
   });
 
-  beforeEach(() => {
+  beforeEach(function () {
     return seedBlogData();
   });
 
-  afterEach(() => {
+  afterEach(function () {
     return tearDownDB();
   });
 
-  after(() => {
+  after(function () {
     return closeServer();
   });
 
-  describe('GET enpoint', () =>{
-
+  describe('GET endpoint', function () {
+    it('should return all posts', function () {
+      let response;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(res) {
+          response = res;
+          res.should.have.status(200);
+          res.body.should.have.length.of.at.least(1);
+          return BlogPost.count();
+        })
+        .then(function(res) {
+          response.body.should.have.length.of(res);
+        })
+    });
+    it('should return posts with necessary fields', function () {
+      let firstPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(function (res) {
+          firstPost = res.body[0];
+          res.body.should.be.an('array');
+          res.should.be.json;
+          res.body[0].should.include.keys('title', 'content', 'author', 'created', 'id');
+          res.body[0].author.should.be.a('string');
+          res.body[0].title.should.be.a('string');
+          res.body[0].content.should.be.a('string');
+          return chai.request(app)
+          .get(`/posts/${firstPost.id}`)
+          .then (function (res) {
+            console.log(res.body);
+            console.log(firstPost);
+            res.body.title.should.equal(firstPost.title);
+            res.body.content.should.equal(firstPost.content);
+            res.body.author.should.equal(firstPost.author);
+            res.body.created.should.equal(firstPost.created);
+            res.body.id.should.equal(firstPost.id);
+          });
+        });
+    });
   });
 
-  describe('POST enpoint', () =>{
+  // describe('POST endpoint', function () {
 
-  });
+  // });
 
-  describe('PUT enpoint', () =>{
+  // describe('PUT endpoint', function () {
 
-  });
-  describe('DELETE enpoint', () =>{
+  // });
+  // describe('DELETE endpoint', function () {
 
-  });
+  // });
 
 })
